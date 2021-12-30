2045ba540d6d1ebc0f06cbd84ffa9147eafab32d
mokotturu <monishkotturu@gmail.com>
2021-12-30 16:50:44 -0500

    testing rebases

--- public/js/new_simulation.js
+++ public/js/new_simulation.js
@@ -22,7 +22,8 @@ var rows, columns, boxWidth, boxHeight;
 const canvasWidth = $map.width();
 const canvasHeight = $map.height();
 
-const gameMode = 'Performance Trust and Moral Trust Base Game';
+const gameMode = 'testing';
+// TESTING
 
 const colors = {
 	human: '#3333ff',
