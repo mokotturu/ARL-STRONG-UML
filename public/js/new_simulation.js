0d8110fc82c6f0ed45201742df6fcdc7085a8848
asgiedraitis <asgiedraitis@gmail.com>
2022-01-11 14:25:27 -0500

    Fixed spacing, slide 5

--- public/js/new_simulation.js
+++ public/js/new_simulation.js
@@ -139,7 +139,7 @@ hazardMarker.src = 'img/hazard-marker-big.png';
 
 const c1_m1 = "I'm sorry I selected circles. I thought they were stars. It's my fault. It won't happen again.";
 
-const c2_m2 = "I just noticed that I made a mistake about which targets to count.  I have fixed the problem. I will select the correct targets from now on.";
+const c2_m2 = "I just noticed that I made a mistake about which targets to count. I have fixed the problem. I will select the correct targets from now on.";
 
 const trustCues = ["X", "X", "X", "X", "X", "X", "X", c1_m1, "X", c2_m2,"X","X","X","X"];
 
