379c0a45ed15cd90b85a1e1bb5be54bea55d7356
asgiedraitis <asgiedraitis@gmail.com>
2022-02-15 11:45:10 -0500

    Changed a cue message, slide 7

--- public/js/new_simulation.js
+++ public/js/new_simulation.js
@@ -137,7 +137,7 @@ hazardMarker.src = 'img/hazard-marker-big.png';
 */
 
 
-const c1_m1 = "I'm sorry I selected circles. I thought they were stars. It's my fault. It won't happen again.";
+const c1_m1 = "I wasn't selecting circles because I didn't want to negatively impact the team score. I don't know what happened.";
 
 const c2_m2 = "I just noticed that I made a mistake about which targets to count. I have fixed the problem. I will select the correct targets from now on.";
 
